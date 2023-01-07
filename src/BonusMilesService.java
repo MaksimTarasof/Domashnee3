afcfa579f55219b5cdca450953cb8527b6ec69ee
Max <eparis@atlassian.com>
2023-01-07 11:47:15 +0400

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int miles = cost/20;
+        int miles = cost / 20;
         if (cost > 20) {
             miles = cost / 20;
         }
